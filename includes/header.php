3d9704baa15a7cbc926778b73cde603ab7e098aa
Plk-3 <perrbear0519@gmail.com>
2026-07-08 08:04:33 -0400

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,19 @@
+<header>
+    <a href="https://ccse.kennesaw.edu/" target="_blank" rel="noopener noreferrer">
+        <img src="img/logo.png"
+             alt="Kennesaw State University College of Computing and Software Engineering Logo"
+             class="logo">
+    </a>
 
+    <h1>Robin and Doug Shore Innovation Center</h1>
+
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="labs.html">Facilities & Labs</a></li>
+            <li><a href="research.html">Research & Opportunities</a></li>
+            <li><a href="eligibility.html">Eligibility Evaluator</a></li>
+            <li><a href="application.php">Research Assistant Application</a></li>
+        </ul>
+    </nav>
+</header>
