a0bb51eb7bbead01814fda074b50a2d2928e77b2
Plk-3 <perrbear0519@gmail.com>
2026-07-08 08:05:42 -0400

    Add footer with project details and KSU link
    Added footer with project information and official links.

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,13 @@
-
+<footer>
+    <p>
+        This website is a student class project for IT 5443 Web Technologies
+        at Kennesaw State University. It is not an official KSU website.
+        For official information about the Interdisciplinary STEM building /
+        Robin and Doug Shore Innovation Center, please visit
+        <a href="https://www.kennesaw.edu/news/stories/2024/ksu-breaks-ground-interdisciplinary-stem-building.php"
+           target="_blank"
+           rel="noopener noreferrer">
+            KSU official news page about the Interdisciplinary STEM building
+        </a>.
+    </p>
+</footer>
